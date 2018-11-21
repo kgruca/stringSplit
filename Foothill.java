988fc4422d9930a5d7e29c73a326360439cdc807
theHackCat <36906818+theHackCat@users.noreply.github.com>
2018-11-21 10:45:24 -0800

    Add files via upload

---
+++ Foothill.java
@@ -0,0 +1,13 @@
+public class Foothill
+{
+   public static void main(String[] args)
+   {
+      String abc = "Harry Potter";
+      String[] arr = abc.split(" ");
+      
+      for(int k = 0; k < arr.length; k++)
+         System.out.println(arr[k]);
+      
+   }
+
+}
